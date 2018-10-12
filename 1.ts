a60e7d1bfb5d854d7da4d8cd22dd110bb6d128eb
zb <2479107942@qq.com>
2018-10-12 23:58:18 +0800

    添加ts

--- 1.ts
+++ 1.ts
@@ -1,1 +1,19 @@
 console.log("hello")
+
+
+interface Person{
+    姓:string;
+    名:string;
+}
+
+function greeter(person:Person){
+    return "hello,"+person.姓+""+person.名
+}
+
+declare var document;
+
+let user={
+    姓:"z",
+    名:"b"
+}
+document.body.innerHTML=greeter(user)
